c6707d1c64ada3064d5447f3ac2b54556828f9c0
michelangelo17 <michelangelo.markus@gmail.com>
2019-12-08 22:04:31 -0800

    added custom hook for local storage stretch goal all stretch goals complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Route } from 'react-router-dom'
 import data from './data'
 import { ProductContext, CartContext } from './contexts'
+import useLocalStorage from './useLocalStorage'
 // Components
 import Navigation from './components/Navigation'
 import Products from './components/Products'
@@ -9,8 +10,8 @@ import ShoppingCart from './components/ShoppingCart'
 
 function App() {
   const [products] = useState(data)
-  const [cart, setCart] = useState([])
-
+  const [cart, setCart] = useLocalStorage('storedData', [])
+	console.log(cart, setCart)
   const addItem = item => {
     setCart([...cart, item])
   }
